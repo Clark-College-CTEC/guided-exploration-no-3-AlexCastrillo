fd8f2b608447d2effb15b0598fd7e56040fb8402
Alex Castrillo <alxcstr30@gmail.com>
2020-08-22 14:41:30 -0700

    Ready for Regrading

--- rap-name-generator.py
+++ rap-name-generator.py
@@ -17,22 +17,22 @@ with open('rap-names.txt', 'r') as dataFile:
         # each iteration of the file removes the space to have the names be next to each other and added to possible_names
         possible_names.append(name.rstrip())
         # The line below asks the user how many rap names they would like
-        count = int(input('How many rap names would you like to create? '))
-        # The line below asks the user how many parts the rap name should have
-        parts = int(input('How many parts should the name contain? '))
+count = int(input('How many rap names would you like to create? '))
+# The line below asks the user how many parts the rap name should have
+parts = int(input('How many parts should the name contain? '))
 
 # the line below runs a for loop following the amount that the user requested
-        for i in range(count):
-            # line below is an accumulator for the names generated
-            name_parts = []
+for i in range(count):
+    # line below is an accumulator for the names generated
+    name_parts = []
     # the line below is a for loop for how many parts of the name the user wants
-            for j in range(parts):
-                # the line below adds a name per loop to the name_parts list.
-                name_parts.append(possible_names[random.randint(0, len(possible_names) - 1)])
+    for j in range(parts):
+        # the line below adds a name per loop to the name_parts list.
+        name_parts.append(possible_names[random.randint(0, len(possible_names) - 1)])
 
-        # The line below will take the generated names and list it to the file opened earlier(rap-names-output.txt) separating each part of the name with a space and a line below
-            outputFile.write(f"{' '.join(name_parts)}\n")
-        # The line below prints out each generated names into the python preview
-            print(f"{' '.join(name_parts)}")
+    # The line below will take the generated names and list it to the file opened earlier(rap-names-output.txt) separating each part of the name with a space and a line below
+    outputFile.write(f"{' '.join(name_parts)}\n")
+# The line below prints out each generated names into the python preview
+    print(f"{' '.join(name_parts)}")
 # The line below closes the file opened in line 11
 outputFile.close()
